40f7327ead6071ecfa74cd5483f40cbce958f781
Alulhilal <hilaluddinj@gmail.com>
2025-04-04 07:58:55 +0700

    menambahkan beberapa file baru

---
+++ fungsi.js
@@ -0,0 +1,20 @@
+console.log("fungsi pertama");
+
+const add = (a, b) => a + b;
+console.log(add(2, 3));
+
+function pertambahan(c, d) {
+  return c + d;
+}
+
+pertambahan(9, 10);
+
+console.log("hello world");
+const phi = 3.14;
+console.log(phi);
+
+const multiply = (x, y) => x * y;
+console.log(multiply(2, 8));
+
+const divided = (x, y) => x / y;
+console.log(divided(8, 2));
---
+++ object.js
@@ -0,0 +1,18 @@
+console.log("ini file object");
+
+const mahasiswa = {
+  nama: "Hilaluddin",
+  NIM: 11190930000088,
+  Domisili: "Jakarta",
+};
+
+console.log(mahasiswa);
+const a = 99;
+console.log(a);
+
+// menambahkan fitur perkalian dan pertambahan
+const perkalian = (x, y) => x * y;
+const pertambahan = (x, y) => x + y;
+
+console.log(perkalian(5, 5));
+console.log(pertambahan(2, 5));
